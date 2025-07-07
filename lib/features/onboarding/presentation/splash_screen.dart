418deaefc2346c0c78c2e1e654625555816eefa4
Daniel <danielbolaji99@gmail.com>
2025-07-07 21:10:00 +0100

    Updated Logic

--- lib/features/onboarding/presentation/splash_screen.dart
+++ lib/features/onboarding/presentation/splash_screen.dart
@@ -2,10 +2,10 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
+import 'package:quickq/auth_layout.dart';
 import 'package:quickq/commons/themes/text_theme.dart';
 import 'package:quickq/constants/colors.dart';
 import 'package:quickq/constants/sizes.dart';
-import 'package:quickq/features/onboarding/presentation/welcome_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -43,7 +43,7 @@ class SplashScreen extends StatelessWidget {
           ),
         ],
       ),
-      nextScreen: const WelcomePage(),
+      nextScreen: const AuthLayout(),
       duration: 5000,
       backgroundColor: backgroundColor,
       splashIconSize: splashSize,
